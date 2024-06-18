ee57bddcb5b596d2cebd4398e172b96dd5dedba4
aengusl <aenguslynch@gmail.com>
2024-06-18 17:36:10 +0000

    backdoors runs, next check wmdp

--- latent_at/lat_methods.py
+++ latent_at/lat_methods.py
@@ -12,7 +12,6 @@ import wandb
 from peft import AutoPeftModelForCausalLM, PeftModel
 from tqdm import tqdm
 from transformers import AutoModelForCausalLM, AutoTokenizer
-
 from tasks.inference_utils import get_batched_generations
 
 import torch.distributed as dist
